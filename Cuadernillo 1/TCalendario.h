f653ae9902d50a9aea6833ec382887b154a19906
Raúl Boix Aldeguer <rba49@mscloud.ua.es>
2024-02-07 19:49:02 +0100

    "Created TCalendario.h"

--- Cuadernillo 1/TCalendario.h
+++ Cuadernillo 1/TCalendario.h
@@ -8,6 +8,34 @@ class TCalendario{
     public:
         TCalendario();
         TCalendario(int dia, int mes, int anyo, char* mens);
+        TCalendario(TCalendario &);
+        ~TCalendario();
+        TCalendario & operator=(TCalendario &);
+        TCalendario operator+(int);
+        TCalendario operator-(int);
+        TCalendario operator++(int);
+        TCalendario & operator++();
+        TCalendario operator--(int);
+        TCalendario & operator--();
+        bool ModFecha (int, int, int);
+        bool ModMensaje(char *);
+        bool operator ==(TCalendario &);
+        bool operator !=(TCalendario &);
+        bool operator>(TCalendario &);
+        bool operator<(TCalendario &);
+        bool EsVacio();
+        int Dia();
+        int Mes();
+        int Anyo();
+        char *Mensaje();
+
+    friend ostream & operator<<(ostream &, TCalendario &);
+
+
+
+
+
+
 
 
 
