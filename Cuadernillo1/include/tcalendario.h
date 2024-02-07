4e4f1bb7b9674fd3871a8b3bc6ddb12d002162de
Raúl Boix Aldeguer <rba49@mscloud.ua.es>
2024-02-07 23:04:30 +0100

    "tcalendario.h done"

--- Cuadernillo1/include/tcalendario.h
+++ Cuadernillo1/include/tcalendario.h
@@ -2,7 +2,6 @@
 // Created by Raúl on 07/02/2024.
 //
 
-#ifndef CUADERNILLO1_TCALENDARIO_H
 #define CUADERNILLO1_TCALENDARIO_H
 
 #ifndef CUADERNILLO_1_TCALENDARIO_H
@@ -10,35 +9,58 @@
 #include <iostream>
 #include <string.h>
 
-class TCalendario{
+class TCalendario {
 private:
     int dia, mes, anyo;
-    char* mensaje;
+    char *mensaje;
+    bool comprobarFecha (int, int, int);
 public:
     TCalendario();
-    TCalendario(int dia, int mes, int anyo, char* mens);
+
+    TCalendario(int dia, int mes, int anyo, char *mens);
+
     TCalendario(TCalendario &);
+
     ~TCalendario();
-    TCalendario & operator=(TCalendario &);
+
+    TCalendario &operator=(TCalendario &);
+
     TCalendario operator+(int);
+
     TCalendario operator-(int);
+
     TCalendario operator++(int);
-    TCalendario & operator++();
+
+    TCalendario &operator++();
+
     TCalendario operator--(int);
-    TCalendario & operator--();
-    bool ModFecha (int, int, int);
+
+    TCalendario &operator--();
+
+    bool ModFecha(int, int, int);
+
     bool ModMensaje(char *);
-    bool operator ==(TCalendario &);
-    bool operator !=(TCalendario &);
+
+    bool operator==(TCalendario &);
+
+    bool operator!=(TCalendario &);
+
     bool operator>(TCalendario &);
+
     bool operator<(TCalendario &);
+
     bool EsVacio();
+
     int Dia();
+
     int Mes();
+
     int Anyo();
+
     char *Mensaje();
 
-    friend ostream & operator<<(ostream &, TCalendario &);
+    friend std::ostream &operator<<(std::ostream &, TCalendario &);
+};
 
 
 #endif //CUADERNILLO1_TCALENDARIO_H
