ed4c2cc94ba8b48ecd0b95beb8c89f8ec243ec79
Raúl Boix Aldeguer <rba49@mscloud.ua.es>
2024-02-07 19:35:27 +0100

    "Created makefile and TCalendario.h"

---
+++ Cuadernillo 1/TCalendario.h
@@ -0,0 +1,15 @@
+#ifndef CUADERNILLO_1_TCALENDARIO_H
+#define CUADERNILLO_1_TCALENDARIO_H
+
+class TCalendario{
+    private:
+        int dia, mes, anyo;
+        char* mensaje;
+    public:
+        TCalendario();
+
+
+
+};
+
+#endif //CUADERNILLO_1_TCALENDARIO_H
