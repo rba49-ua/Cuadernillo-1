7a0033fc5008fbfa8dbde017de68146807e57185
Raúl Boix Aldeguer <rba49@mscloud.ua.es>
2024-02-20 12:37:02 +0100

    "implemented TNodoCalendario"

--- Cuadernillo1/lib/tlistacalendario.cpp
+++ Cuadernillo1/lib/tlistacalendario.cpp
@@ -1,1 +1,37 @@
+#include "../include/tlistacalendario.h"
+
+using namespace std;
+
+void TNodoCalendario::Copia(const TNodoCalendario &n) {
+    this->c = n.c;
+    this.siguiente = n.siguiente;
+}
+
+// También se puede poner:
+// TNodoCalendario::TNodoCalendario(): c(), siguiente(NULL){}
+TNodoCalendario::TNodoCalendario() {
+    c.TCalendario();
+    siguiente=NULL;
+}
+
+TNodoCalendario::TNodoCalendario(const TNodoCalendario &n) {
+    Copia(n);
+}
+
+TNodoCalendario::~TNodoCalendario() {
+    c.~TCalendario();
+    siguiente=NULL;
+}
+
+TNodoCalendario &TNodoCalendario::operator=(const TNodoCalendario &n) {
+    if(this == &n){
+        return *this;
+    }
+
+    (*this).~TNodoCalendario();
+    Copia(n);
+    return *this;
+
+}
+
 
