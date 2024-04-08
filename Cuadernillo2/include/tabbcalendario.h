1a5a27eefebd4c8c713530b14c5a95a9da498ea0
Raúl Boix Aldeguer <rba49@mscloud.ua.es>
2024-04-08 13:53:52 +0200

    some bugs fixed

--- Cuadernillo2/include/tabbcalendario.h
+++ Cuadernillo2/include/tabbcalendario.h
@@ -1,6 +1,8 @@
 //
 // Created by Raúl on 08/04/2024.
 //
+#include <iostream>
+using namespace std;
 
 #ifndef CUADERNILLO2_TABBCALENDARIO_H
 #define CUADERNILLO2_TABBCALENDARIO_H
@@ -13,11 +15,11 @@ class TNodoABB;
 class TABBCalendario{
     friend ostream & operator<<(ostream &, TABBCalendario &);
     private:
+        void Copia(const TABBCalendario &);
         TNodoABB *raiz;
         void InordenAux(TVectorCalendario &, int &) const;
         void PreordenAux(TVectorCalendario &, int &) const;
         void PostordenAux(TVectorCalendario &, int &) const;
-        void Copia(const TABBCalendario &);
 
     public:
         TABBCalendario();
@@ -47,15 +49,17 @@ class TABBCalendario{
 };
 
 class TNodoABB{
-private:
-    TCalendario item;
-    TABBCalendario iz, de;
+    friend class TABBCalendario;
+    private:
+        void Copia(const TNodoABB &);
+        TCalendario item;
+        TABBCalendario iz, de;
 
-public:
-    TNodoABB();
-    TNodoABB(const TNodoABB &);
-    ~TNodoABB();
-    TNodoABB & operator=(const TNodoABB &);
+    public:
+        TNodoABB();
+        TNodoABB(const TNodoABB &);
+        ~TNodoABB();
+        TNodoABB & operator=(const TNodoABB &);
 };
 
 #endif //CUADERNILLO2_TABBCALENDARIO_H
