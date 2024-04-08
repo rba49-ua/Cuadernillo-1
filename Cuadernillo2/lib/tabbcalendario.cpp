31d7722e7fbd588aca9b4883b205efcc38496532
Raúl Boix Aldeguer <rba49@mscloud.ua.es>
2024-04-08 13:54:15 +0200

    implemented some methods

--- Cuadernillo2/lib/tabbcalendario.cpp
+++ Cuadernillo2/lib/tabbcalendario.cpp
@@ -1,3 +1,86 @@
-//
-// Created by Raúl on 08/04/2024.
-//
+#include "../include/tabbcalendario.h"
+
+void TNodoABB::Copia(const TNodoABB &other){
+    item = other.item;
+    iz = other.iz;
+    de = other.de;
+}
+
+TNodoABB::TNodoABB( ):item(), iz(),de(){}
+
+TNodoABB::TNodoABB(const TNodoABB &other) {
+    Copia(other);
+}
+
+TNodoABB::~TNodoABB() {}
+
+TNodoABB &TNodoABB::operator=(const TNodoABB &other){
+    if(this != &other){
+        (*this).~TNodoABB();
+        Copia(other);
+    }
+    return *this;
+}
+
+
+void TABBCalendario::Copia(const TABBCalendario &origen) {
+    if (origen.raiz != NULL){
+        TNodoABB *aux = new TNodoABB( );
+        aux->item = origen.raiz->item;
+        raiz = aux;
+        raiz-> de.Copia(origen.raiz->de);
+        raiz->iz.Copia(origen.raiz->de);
+    }
+    else raiz = NULL;
+}
+
+TABBCalendario::TABBCalendario(){
+    raiz = NULL;
+}
+
+TABBCalendario::TABBCalendario(const TABBCalendario &other){
+    Copia(other);
+}
+
+TABBCalendario::~TABBCalendario(){
+    TNodoABB *aux= new TNodoABB();
+
+    while (!EsVacio()){
+        aux = raiz;
+        raiz-> de.~TABBCalendario();
+        raiz->iz.~TABBCalendario();
+        delete raiz;
+        raiz = NULL;
+    }
+}
+
+TABBCalendario &TABBCalendario::operator= (const TABBCalendario & abb){
+    if(this != &abb){
+        this -> ~TABBCalendario( );
+        Copia(abb);
+    }
+    return *this;
+}
+
+bool TABBCalendario::operator== (const TABBCalendario &abb) const {
+    TVectorCalendario a = abb.Inorden();
+    return Inorden() == a;
+}
+
+bool TABBCalendario::EsVacio() const {
+    return this->raiz==NULL;
+
+}
+
+TVectorCalendario TABBCalendario::Inorden() const {
+
+
+}
+
+
+
+
+
+
+
+
